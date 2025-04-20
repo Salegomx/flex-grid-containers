fa1144802648e19c4c9d5a1a5ea456f55e240448
Sergio Gómez <sergioagmoreno@gmail.com>
2025-04-20 17:28:54 -0500

    Added container queries, improved handling grid columns

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,18 @@
-flexContainers = document.querySelectorAll('.flex');
+flexContainers = document.querySelectorAll('[data-layout="flow"]');
 
 flexContainers.forEach((container) => {
-    const gap = container.getAttribute('gap');
-    const direction = container.getAttribute('direction');
-    const wrap = container.getAttribute('wrap');
-    const align = container.getAttribute('align');
-    const valign = container.getAttribute('valign');
+    const gap = container.getAttribute('data-gap');
+    const direction = container.getAttribute('data-direction');
+    const wrap = container.getAttribute('data-wrap');
+    const align = container.getAttribute('data-align');
+    const valign = container.getAttribute('data-valign');
+    const width = container.getAttribute('data-width');
     
     if (gap) container.style.setProperty('--flex-gap', gap);
     if (direction) container.style.setProperty('--flex-direction', direction);
     if (align) container.style.setProperty('--justify-content', align);
     if (valign) container.style.setProperty('--align-items', valign);
+    if(width) container.style.setProperty('--flex-width', width);
     if (wrap)
         wrap === 'no' ? container.style.setProperty('--flex-wrap', 'nowrap')
         : wrap === 'yes' ? container.style.setProperty('--flex-wrap', 'wrap')
@@ -21,30 +23,57 @@ flexContainers.forEach((container) => {
     if (children.lenght = 0) return;
 
     Array.from(children).forEach((child) => {
-        const grow = child.getAttribute('grow');
-        const shrink = child.getAttribute('shrink');
-        const order = child.getAttribute('order');
-        const width = child.getAttribute('width');
+        const grow = child.getAttribute('data-grow');
+        const shrink = child.getAttribute('data-shrink');
+        const order = child.getAttribute('data-order');
+        const childWidth = child.getAttribute('data-width');
 
         if (grow) child.style.setProperty('--grow', grow);
         if (shrink) child.style.setProperty('--shrink', shrink);
-        if (width) child.style.setProperty('--flex-basis', width);
+        if (childWidth) child.style.setProperty('--flex-basis', childWidth);
         if (order) child.style.setProperty('--order', order);
     })
 });
 
-gridContainers = document.querySelectorAll('.grid');
+gridContainers = document.querySelectorAll('[data-layout="grid"]');
 
 gridContainers.forEach((container) => {
-    const gap = container.getAttribute('gap');
-    const columns = container.getAttribute('columns');
-    const column_width = container.getAttribute('column-width');
+    /* turn parent into CSS container */
+    const parent = container.parentElement;
+    parent.style.setProperty('container-type', 'inline-size');
+
+    const gap = container.getAttribute('data-gap');
+    const columns = container.getAttribute('data-columns');
+    const width = container.getAttribute('data-width');
 
     if (gap) container.style.setProperty('--grid-gap', gap);
-    if (columns) container.style.setProperty('--columns', columns);
-    if (column_width) container.style.setProperty('--column-width', column_width);
-
+    if (columns) container.style.setProperty('--template-columns', getTemplateColumns(columns));
+    if(width) container.style.setProperty('--grid-width', width);
+    
     /* child nodes */
     const children = container.children;
     if (children.lenght = 0) return;
+
+    Array.from(children).forEach((child) => {
+        const span = child.getAttribute('data-span');
+
+        if (span) child.style.setProperty('--span', span);
+    })
 })
+
+/* Helper functions */
+function getTemplateColumns(str) {
+    console.log(str)
+    return str
+        .split(' ')
+        .map(val => {
+            if (val === 'auto') {
+                return '1fr';
+            } else if (!isNaN(val)) {
+                return val+'fr';
+            } else {
+                return val;
+            }
+        })
+        .join(' ');
+};
